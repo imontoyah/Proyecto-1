19fd028c92b11e3cdfefa4283c6e8ec79914ee22
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-02-03 12:31:28 -0500

    Update Euclides.py

--- talleres/taller02/Euclides.py
+++ talleres/taller02/Euclides.py
@@ -4,9 +4,10 @@ def euclides(a,b):
 
     if minimo == 0:
         return maximo
-
     elif minimo == 1:
-        return 1
-        
+        return 1       
     else :
         return euclides(minimo, maximo % minimo)
+    
+print(euclides(12, 36))
+print(euclides(8, 12))

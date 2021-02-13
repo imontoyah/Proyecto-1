9e068e3d1843db4a07a9fd6b85e1f716af549e41
Maria Isabel Arango Palacio <73366273+miarango@users.noreply.github.com>
2021-02-13 10:21:20 -0500

    Add files via upload

---
+++ proyecto/codigo/Proyecto.py
@@ -0,0 +1,13 @@
+import numpy as np
+import glob
+from PIL import Image
+
+def matriz_csv(n_archivo_csv):
+    matriz_csv_image = np.loadtxt(open(n_archivo_csv, "rb"), delimiter=",").astype(int)
+
+    return matriz_csv_image
+
+csv_files = glob.glob('*.csv')
+matriz_csv_var = matriz_csv(csv_files[0])
+image = Image.fromarray(matriz_csv_var)
+image.show()

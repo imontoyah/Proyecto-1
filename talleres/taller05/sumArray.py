50aae9fcb894a24dbebcebc459848a4353f84dc8
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-02-24 17:53:10 -0500

    Update sumArray.py

--- talleres/taller05/sumArray.py
+++ talleres/taller05/sumArray.py
@@ -37,3 +37,43 @@ for i in range(0,10000,500):
     total=fin-inicio
     times.append(total)
 plot(times,21,"SumaArray")
+
+-------------- Suma recursiva---------------
+from time import time 
+import random
+import matplotlib.pyplot as plt 
+ 
+def array_generator(len):
+    array = [0] * len
+    for i in range(len):
+        array[i] = random.randrange(0,100)
+    return array
+ 
+def sumArray(arr, n):
+    tamano = n #c1 = 1
+    if(tamano==0): #c2 = 4
+        return arr[tamano]
+    elif tamano > 0:
+        return arr[tamano-1] + sumArray(arr, tamano-1) #c3 + T(n-1)
+        #T(n) = c3 + T(n-1)
+        #T(n) = T(n-1)
+        # O(n) = n
+times = []
+ 
+def plot(times,n,lab):
+    plt.xlabel('Dimension del problema')
+    plt.ylabel('Tiempo de complejidad')
+    plt.plot(range(1,n),times,label=lab)
+    plt.grid()
+    plt.legend()
+    plt.show()
+    
+ 
+for i in range(1000,1021):
+    array = array_generator(i)
+    inicio=time()
+    sumArray(array, (len(array)-1))
+    fin=time()
+    total=fin-inicio
+    times.append(total)
+plot(times,21,"SumaArregloRecursiva")

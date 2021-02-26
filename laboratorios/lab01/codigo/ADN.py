fbfc79f460008f6aa2a1f582d8b32e623855d043
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-02-26 15:16:30 -0500

    Update ADN.py

--- laboratorios/lab01/codigo/ADN.py
+++ laboratorios/lab01/codigo/ADN.py
@@ -4,18 +4,19 @@ import random
 import string
 string.ascii_letters = "abcdefghijqlmnokrstuvwxyz"
 
-def subsecuencia(x , y):
+def subsequence(x , y):
     i = len(x) #T(n,m) = C1 donde C1 = 2
     j = len(y) #T(n,m) = C2 donde C2 = 2
-    if(i == 0 or j == 0): #T(n,m) = C3 donde C3 = 4
+    if(i == 0 or j == 0): #T(n,m) = C3 where C3 = 4  /  T(p) = C3  where p = m + n
         return 0 
-    elif(x[i-1] == y[j-1]): ##T(n,m) = C4 + T(n-1, m-1) 
-        return 1 + subsecuencia(x[:i-1],y[:j-1]) 
+    elif(x[i-1] == y[j-1]): ##T(n,m) = C4 + T(n-1, m-1)  / T(p) = C4 + T(p-1) where p = m + n
+        return 1 + subsequence(x[:i-1],y[:j-1]) 
     else:
-        return max(subsecuencia(x[:i-1],y),subsecuencia(x,y[:j-1])) #T(n,m)= C5 + T(n-1, m) + T(n,m-1)
+        return max(subsequence(x[:i-1],y),subsequence(x,y[:j-1])) #T(n,m)= C5 + T(n-1, m) + T(n,m-1)  /  T(p) = C5 + T(p-1) + T(p-1) where p = m + n
 """
-    T(n)= c5 + T(n-1) + T(n-1)
-    Recurrence equation solution : c5 (2^n-1)+c1 2^n-1
+    Worst case:
+    T(n,m)= C5 + T(n-1, m) + T(n,m-1) ,
+    Recurrence equation solution : -T(n-1, m) - T(n, m-1) + T(n,m)
     O(2^n)
 
 """
@@ -32,17 +33,17 @@ def plot(times,n,lab):
 
 final = 1
 for i in range (1,18):
-    cadena1 = ""
-    cadena2 = ""
+    string1 = ""
+    string2 = ""
     for j in range(1, final):
         cadena1 += random.choice(string.ascii_letters)
         cadena2 += random.choice(string.ascii_letters)
     final = final + 1
-    inicio=time()
-    subsecuencia(cadena1,cadena2)
+    start=time()
+    subsequence(string1,string2)
     fin = time()
-    total=fin-inicio
+    total=fin-start
     times.append(total)
-plot(times,18,"Subsecuence")
+plot(times,18,"Subsequence")
 
 

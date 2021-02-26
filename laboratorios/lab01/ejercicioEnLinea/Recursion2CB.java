de6c2f6f7b01787076e61b835edfbb2ef4f84b13
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-02-26 09:09:53 -0500

    Add files via upload

---
+++ laboratorios/lab01/ejercicioEnLinea/Recursion2CB.java
@@ -0,0 +1,61 @@
+public boolean groupSum6(int start, int[] nums, int target) {
+    if(start >= nums.length){   //C1 = 5 = T(0)
+      if(target==0){
+        return true;
+      }
+    }
+    else if(nums[start]==6){                      // T(n)= c2 + T(n-1)   C2 = 6
+      return groupSum6(start+1, nums, target-6);  // T(n) = C*n +C1
+    }
+    else{
+      return groupSum6(start+1, nums, target) || groupSum6(start+1, nums, target-nums[start]);   // T(n) = C2 + T(n-1) + T(n-1)   C3= C2
+    }                                                                                            // T(n)= C(2^n-1) + C1* 2^n-1
+    return false;   //C4=1
+  }
+
+#%%
+public boolean groupNoAdj(int start, int[] nums, int target) {
+    if(start >= nums.length){  // C1 = 6 ?
+      if(target==0){
+        return true;
+      }
+    }
+    
+    else{
+    return groupNoAdj(start+1, nums, target) || groupNoAdj(start+2, nums, target-nums[start]);  // T(n) = T(n-1) + T(n-2) + C2     C2=2
+  }                                                                                             // T(n) = -C + C1*F_n + C2*L_n 
+   return false;          // C3 = 1
+  }
+
+#%%
+public boolean groupSum5(int start, int[] nums, int target) {
+    if(start >= nums.length){  // C1 = 6
+      if(target==0){
+        return true;
+      } else{
+        return false;       //C2 = 2
+      }
+    }
+    if(nums[start]==1 && start > 0 && nums[start-1]%5==0){  // T(n) = C3 + T(n-1)         C3 = 12
+      return groupSum5(start+1, nums, target);
+    }
+    else if(nums[start]%5==0){                             // T(n) = C4 + T(n-1)         C4 = 7
+      return groupSum5(start+1, nums, target-nums[start]); // T(n) = C*n + C1 
+    }
+    else{                                                                                            // T(n) = C5 + T(n-1) + T(n-1)    C5 = 6
+      return groupSum5(start+1, nums, target) || groupSum5(start+1, nums, target-nums[start]);       // T(n)= C(2^n-1) + C1* 2^n-1
+    }
+  }
+
+#%%
+public boolean splitArray(int[] nums) {
+    return splitArrayAux(0,nums,0,0);
+}
+private boolean splitArrayAux(int start,int[] nums,int targetA, int targetB) {
+ if(start >= nums.length){ // C0.1 = 5 
+     return targetA == targetB; //C1 = 7
+ }else{                 
+     int x = nums[start];     //C2 = 10
+     return splitArrayAux(start+1 ,nums, targetA+ x, targetB) || splitArrayAux(start+1 ,nums, targetA,x+ targetB); // T(n) = T(n-1) + T(n-1) +C2
+ }                                                                                                                 // T(n)= C2(2^n-1) +C1*2^(n-1)
+}

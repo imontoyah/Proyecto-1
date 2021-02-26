599dcbed0b1775c28ce783974f1f56d7721a0d28
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-02-26 14:38:30 -0500

    Update ADN.py

--- laboratorios/lab01/codigo/ADN.py
+++ laboratorios/lab01/codigo/ADN.py
@@ -5,14 +5,14 @@ import string
 string.ascii_letters = "abcdefghijqlmnokrstuvwxyz"
 
 def subsecuencia(x , y):
-    i = len(x) #c1
-    j = len(y)  #c2
-    if(i == 0 or j == 0): #c3 = 4
-        return 0 #c5 = 1
-    elif(x[i-1] == y[j-1]): #c6 = 2
-        return 1 + subsecuencia(x[:i-1],y[:j-1]) # T(n)= c4 + T(n-1)
+    i = len(x) #T(n,m) = C1 donde C1 = 2
+    j = len(y) #T(n,m) = C2 donde C2 = 2
+    if(i == 0 or j == 0): #T(n,m) = C3 donde C3 = 4
+        return 0 
+    elif(x[i-1] == y[j-1]): ##T(n,m) = C4 + T(n-1, m-1) 
+        return 1 + subsecuencia(x[:i-1],y[:j-1]) 
     else:
-        return max(subsecuencia(x[:i-1],y),subsecuencia(x,y[:j-1])) #T(n)= c5 + T(n-1) + T(n-1)
+        return max(subsecuencia(x[:i-1],y),subsecuencia(x,y[:j-1])) #T(n,m)= C5 + T(n-1, m) + T(n,m-1)
 """
     T(n)= c5 + T(n-1) + T(n-1)
     Recurrence equation solution : c5 (2^n-1)+c1 2^n-1

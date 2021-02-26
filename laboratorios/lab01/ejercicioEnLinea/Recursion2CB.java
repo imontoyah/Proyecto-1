427dfe84cfec417e6256ddd6396f83067ede2c70
Maria Isabel Arango Palacio <73366273+miarango@users.noreply.github.com>
2021-02-26 10:42:48 -0500

    Update Recursion2CB.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion2CB.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion2CB.java
@@ -58,4 +58,25 @@ private boolean splitArrayAux(int start,int[] nums,int targetA, int targetB) {
      int x = nums[start];     //C2 = 10
      return splitArrayAux(start+1 ,nums, targetA+ x, targetB) || splitArrayAux(start+1 ,nums, targetA,x+ targetB); // T(n) = T(n-1) + T(n-1) +C2
  }                                                                                                                 // T(n)= C2(2^n-1) +C1*2^(n-1)
-}
+
+    
+#%%
+public  boolean groupSumClump(int start, int[] nums, int target) {
+     if(target == 0 && start == nums.length ) 
+          return true;                           //C1 = 6 
+      else if (target !=0 && start == nums.length) 
+           return false;                           // C2 = 10
+      else if(start == nums.length-1)                                              
+            return groupSumClump(start +1 ,nums, target-nums[start]) || groupSumClump(start +1 ,nums, target); // C3 = 8      T(n) = T(n-1)+T(n-1) + C3
+                                                                                                                //            T(n)=C3(2^n-1) +C1*2^(n-1)  
+    
+           
+       else if(nums[start] == nums[start+1]  && -start+nums.length-1 >= 1)                                     
+            return groupSumClump( start+2, nums,  target-2*nums[start])||groupSumClump( start+2, nums,target); //c4 = 16      T(n) = T(n-2) + T(n-2) + C4
+                                                                                                               //             T(n)=2^(1/2)*(C2(-1)^(n)+ C1) - c4
+             
+      else 
+         return groupSumClump(start +1 ,nums, target-nums[start]) || groupSumClump(start +1 ,nums, target);   // C5 = 6       T(n) = T(n-1)+T(n-1) + C_5 
+                                                                                                             //               T(n)=C5(2^n-1) +C1*2^(n-1)
+        
+    }

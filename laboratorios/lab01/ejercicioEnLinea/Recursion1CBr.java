54a9602732f6005553d606487112f1041621e7c6
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-02-26 15:27:14 -0500

    Update Recursion1CBr.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion1CBr.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion1CBr.java
@@ -26,16 +26,12 @@ public int bunnyEars2(int bunnies)
 	return 3 + bunnyEars2(bunnies-1);
 }
 
-public int count7(int n) {
-    if(n == 7){
-     return 1;
-   }else if(n < 10 && n != 7){
-     return 0;
-   }
-   else{
-     return count7(n%10)+count7(n/10);// T(n)= c + T(n%10) + T(n/10)
-   }
- }
+public int sumDigits(int n) {
+  if(n < 10){     
+	return n;
+  }
+	return sumDigits(n/10) + n%10;
+}
 
  public int powerN(int base, int n) {
   if(n==1){ 

95c18d076ee520f1cf231e00e13ef4e7fe8889e1
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-02-27 08:09:59 -0500

    Update Rectangulo.py

--- laboratorios/lab01/codigo/Rectangulo.py
+++ laboratorios/lab01/codigo/Rectangulo.py
@@ -1,6 +1,29 @@
-def rectangulos(n):
+from time import time 
+import random
+import matplotlib.pyplot as plt 
+
+def rectangles(n):
     if(n<=2):
         return n
-    return rectangulos(n-1) + rectangulos(n-2)
+    return rectangles(n-1) + rectangles(n-2)
 
-print(rectangulos(5))
+print(rectangles(5))
+
+times = []
+
+def plot(times,n,lab):
+    plt.xlabel('Dimension del problema')
+    plt.ylabel('Tiempo de complejidad')
+    plt.plot(range(1,n),times,label=lab)
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+for i in range(1001,1012):
+    inicio=time()
+    n = random.randrange(0,100)
+    rectangles(n)
+    fin=time()
+    total=fin-inicio
+    times.append(total)
+plot(times,12,"Rectangles")

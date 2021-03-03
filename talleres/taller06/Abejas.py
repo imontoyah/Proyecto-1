1ffb8542fe7da9812e5ac1c485a93264dc5368b0
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-03-03 09:00:59 -0500

    Update Abejas.py

--- talleres/taller06/Abejas.py
+++ talleres/taller06/Abejas.py
@@ -2,24 +2,31 @@ class ArrayList:
 
     __elements = []
 
-    def __init__(self):
+    def __init__(self):               # T(n) = C1
         self.__elements = []
 
-    def size(self):
+    def size(self):                   # T(n) = C2
         return len(self.__elements)
 
-    def get(self, index):
+    def get(self, index):              #T(n)= C3
         return self.__elements[index]
 
-    def add(self, object):
+    def add(self, object):               # T(n) = n
         self.__elements.append(object)
 
-    def addInIndex(self, index, object):
+    def addInIndex(self, index, object):      # T(n) = n
         self.__elements.insert(index, object)
 
-    def removeInIndex(self,index):
+    def removeInIndex(self,index):    # T(n) = n
        self.__elements.pop(index)
     
-arr = ArrayList()
-arr.add(23)
-print(arr.get(0))
+arr = ArrayList()    # T(n) = C4
+arr.add(23)         # T(n) = n
+print(arr.get(0))    # T(n) = C3
+
+"""
+Complejidad 
+T(n) = C4+ C3 + n
+T(n) = n    ---> Regla de la suma 
+O(n)
+"""

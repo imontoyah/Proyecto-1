c0aa2d08030a05c9844a57fff284719340432b22
Maria Isabel Arango Palacio <73366273+miarango@users.noreply.github.com>
2021-03-03 08:00:14 -0500

    Add files via upload

---
+++ talleres/taller06/Abejas.py
@@ -0,0 +1,25 @@
+class ArrayList:
+
+    __elements = []
+
+    def __init__(self):
+        self.__elements = []
+
+    def size(self):
+        return len(self.__elements)
+
+    def get(self, index):
+        return self.__elements[index]
+
+    def add(self, object):
+        self.__elements.append(object)
+
+    def addInIndex(self, index, object):
+        self.__elements.insert(index, object)
+
+    def removeInIndex(self,index):
+       self.__elements.pop(index)
+    
+arr = ArrayList()
+arr.add(23)
+print(arr.get(0))

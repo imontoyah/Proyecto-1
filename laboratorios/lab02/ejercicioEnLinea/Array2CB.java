ed6df8168b1d832a76a9935fbf3116479f9a3e84
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-03-03 09:26:38 -0500

    Update Array2CB.java

--- laboratorios/lab02/ejercicioEnLinea/Array2CB.java
+++ laboratorios/lab02/ejercicioEnLinea/Array2CB.java
@@ -1,3 +1,5 @@
+ 
+-------------- More14 ----------------------
 public boolean more14(int[] nums) {
     int contador1 = 0;
     int contador4 = 0;
@@ -15,13 +17,19 @@ public boolean more14(int[] nums) {
       return false;
   }
   
-  #%%
+-------------- FizzArray ------------------------
 
   public int[] fizzArray(int n) {
-    int[] nuevo = new int[n];
-    for(int i = 0;i<n;i++){
+    int[] nuevo = new int[n];        // T(n) = C1
+    for(int i = 0;i<n;i++){          // T(n) = C2 + n
       nuevo [i] = i;
     }
-    return nuevo;
+    return nuevo;                   // T(n) = C3
   }
+"""
+Complexity for the worst case
+T(n) = C2 + n
+T(n) = n      ----> Sum law
+O(n)  where n is the array`s length
+"""
   

fb1d9ce71730d2f4e72b105a4d44857033615cc7
Maria Isabel Arango Palacio <73366273+miarango@users.noreply.github.com>
2021-03-07 19:28:48 -0500

    Update Array3CB.java

--- laboratorios/lab02/ejercicioEnLinea/Array3CB.java
+++ laboratorios/lab02/ejercicioEnLinea/Array3CB.java
@@ -18,34 +18,48 @@ The complexity for the worst case is a constant
 */
 ---------------------------------------Fix34---------------------------
 public int[] fix34(int[] nums) {
-    int temp = 0;
-    for(int i = 0; i < nums.length; i++){
-        for(int j = 0; j < nums.length; j++){
-          if(nums[j] == 3 && nums[i] == 4 ){
-            temp = nums[j+1];
-            nums[j+1] = nums[i];
+    int temp = 0;                                  //C1 = 1
+    for(int i = 0; i < nums.length; i++){          // T(n) = C2 + C3*n   where C2 = 1 , C3 = 3
+        for(int j = 0; j < nums.length; j++){      //T(n) = (C4 + C5*n)*n    where C4 = 1 , C5 = 3
+          if(nums[j] == 3 && nums[i] == 4 ){      
+            temp = nums[j+1];                      //C6*n*n where C6 = 13
+            nums[j+1] = nums[i];                   
             nums[i] = temp;
           } 
         }
      }
-     return nums;
+     return nums;                                  //C7=1
     
   }
+/* Complexity worst case
+T(n) = C1 + (C2 + C3*n) + [(C4+C5*n)*n] + C6*n*n +C7
+T(n) = C3*n + C5*n*n + C6*n*n            ----> Common factor and sum law
+T(n) = n + n*n                           ----> Product law
+T(n) = n*n                               ----> Sum law
+O(n^2) where n is the aray´s length 
+*/
 ---------------------------------------Fix45---------------------------
     public int[] fix45(int[] nums) {
-  int temp = 0;
+  int temp = 0;                                  //C1 = 1
   
-  for(int i=0; i<nums.length; i++){
-    for(int j=0; j<nums.length; j++){
+  for(int i=0; i<nums.length; i++){              // T(n) = C2 + C3*n   where C2 = 1 , C3 = 3
+    for(int j=0; j<nums.length; j++){            //T(n) = (C4 + C5*n)*n    where C4 = 1 , C5 = 3
       if(nums[j] == 4 && nums[i] == 5){
         temp = nums[j+1];
         nums[j+1] = nums[i];
-        nums[i] = temp;
+        nums[i] = temp;                         //C6*n*n where C6 = 13
     }
    }
  }
- return nums;
+ return nums;                                  //C7=1
 }
+/* Complexity worst case
+T(n) = C1 + (C2 + C3*n) + [(C4+C5*n)*n] + C6*n*n +C7
+T(n) = C3*n + C5*n*n + C6*n*n            ----> Common factor and sum law
+T(n) = n + n*n                           ----> Product law
+T(n) = n*n                               ----> Sum law
+O(n^2) where n is the aray´s length 
+*/
 ---------------------------------------canBalance---------------------------
 public boolean canBalance(int[] nums) {
   int sum1= 0;                           // T1(n) = C1    where C1 = 1
@@ -103,20 +117,27 @@ Where n is the inner`s length and m is the outer`s length
 */
 ---------------------------------------CountClump---------------------------
     public int countClumps(int[] nums) {
-  int count = 0;
+  int count = 0;                                    //C1 = 2
   boolean resultado = false;
   
-  if(nums.length == 0){
+  if(nums.length == 0){                             //C2 = 4
     return count;
   }
-  for(int i = 0; i < nums.length-1; i++){
-    if(nums[i] == nums[i+1] && !resultado){
+  for(int i = 0; i < nums.length-1; i++){           // T(n) = C3 + C4*(n-1)   where C3 = 1 , C4 = 3 
+    if(nums[i] == nums[i+1] && !resultado){         //C5*(n-1)  where C5 = 8
       resultado = true;
       count++;
     }
-    else if(nums[i]!=nums[i+1]){
+    else if(nums[i]!=nums[i+1]){                    //C6*(n-1) where C6 = 5
       resultado = false;
     }
   }
-  return count;
+  return count;                                      //C7 = 1
 }
+/* Complexity worst case
+T(n) = C1 + C2 + (C3 + C4)*(n-1) + C5*(n-1) + C6*(n-1) +C7
+T(n) = C4*(n-1) + C5*(n-1) + C6*(n-1)       ----> Common factor and sum law
+T(n) = n-1                                  ----> Product law
+T(n) = n                                    ----> Sum law
+O(n) where n is the aray´s length 
+*/

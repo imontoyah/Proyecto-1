976da82ff639d067f4c24a7dfd21c9088a921c5a
Maria Isabel Arango Palacio <73366273+miarango@users.noreply.github.com>
2021-03-26 18:54:28 -0500

    Update TecladoRoto.py

--- laboratorios/lab03/ejercicioEnLinea/TecladoRoto.py
+++ laboratorios/lab03/ejercicioEnLinea/TecladoRoto.py
@@ -1,37 +1,43 @@
 from collections import deque    
 class TecladoRoto():
-
     def teclado(self,string):
         texto = deque()                                                                 #C1
         acc = 0     #0 - agregar al final / 1 - agrgar al inicio                        #C2
-        aux = ''                                                                        #C3
-        for i in range (len(string)):                                                   #C3.0*n  - - > where n is the string´s length
+        aux = ''
+        stringFinal = ''                                                                        #C3
+        for i in range (len(string)):                                                   #T1(n) = n  - - > where n is the string´s length
             if string[i] != '[' and string[i] != ']':                                   #C4
                aux = aux + string[i]                                                    #C5
             if string[i] == '[':                                                        #C6
                 if acc == 0:                                                            #C7
-                    texto.append(aux)                                                   #C7.0 - - > because append in a LinkedList is a constant
+                    texto.append(aux)                                                   #T2(n) = m - - > where m is the List´s length
                     aux = ''                                                            #C8
                 else:
-                    texto.extendleft(aux)                                               #C9
+                    texto.insert(0,aux)                                               #C9
                     aux = ''                                                            #C10
                 acc = 1                                                                 #C11
             if string[i] == ']':                                                        #C12
                 if acc == 1:                                                            #C13
-                    texto.extendleft(aux)                                               #C15
+                    texto.insert(0,aux)                                               #C15
                     aux = ''                                                            #C16
                 acc = 0                                                                 #c17
             if i == len(string)-1:                                                      #C18
                 if acc == 0:                                                            #C19
-                    texto.append(aux)                                                   #C19.0  - - > because append in a LinkedList is a constant
+                    texto.append(aux)                                                   #T3(n) = m - - > where m is the List´s length
                     aux = ''                                                            #C20
                 else:
-                    texto.extendleft(aux)                                               #C21           
-                    aux = ''                                                            #C22
-        return (texto)                                                                  #C23*m - - > where m is the List´s length
+                    texto.insert(0,aux)                                               #C21           
+                    aux = '' 
+                    
+                for i in range(len(texto)):
+                    stringFinal += texto.popleft()
+
+        return stringFinal                                                    #C22
+                                                                                  #T4(n) = m - - > where m is the List´s length
     
 teclado = TecladoRoto()
-print(TecladoRoto.teclado(TecladoRoto,'asd[gfh[[dfh]hgh]fdfhd[dfg[d]g[d]d'))
+print(TecladoRoto.teclado(TecladoRoto,'This_is_a_[Beiju]_text'))
+
 """
 1.T(n) = C + C23*m  + C3.0*n 
 2.T(n) = C23*m  + C3.0*n                     ---> Sum law,   m=n
